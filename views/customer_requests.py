50cc398f4e263345b4c9f1baf50a64bd750d5b98
Derek Malone <laxmalone@gmail.com>
2023-04-06 15:00:32 -0500

    completed chpt 4

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,12 +1,9 @@
-CUSTOMERS = [
-    {
-        "id": 1,
-        "name": "Ryan Tanay"
-    }
-]
+CUSTOMERS = [{"id": 1, "name": "Ryan Tanay"}]
+
 
 def get_all_customers():
     return CUSTOMERS
+
 
 def get_single_customer(id):
     requested_customer = None
@@ -15,3 +12,11 @@ def get_single_customer(id):
             requested_customer = customer
 
     return requested_customer
+
+
+def create_customer(customer):
+    max_id = CUSTOMERS[-1]["id"]
+    new_id = max_id + 1
+    customer["id"] = new_id
+    CUSTOMERS.append(customer)
+    return customer
