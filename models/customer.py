685628ca5bc7c0f45081d732e461286a0f77aeac
Derek Malone <laxmalone@gmail.com>
2023-04-10 15:57:16 -0500

    completed chpt 7

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    """A member of Kennels. Stores information on a customer 
+    that is currently pertinent.
+    """
+    def __init__(self, id, name):
+        self.id = id
+        self.full_name = name
