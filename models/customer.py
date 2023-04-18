f8ce66ece4f58d87dc6debb4f77cb8f56736d7ae
Derek Malone <laxmalone@gmail.com>
2023-04-17 21:04:28 -0500

    saving and moving to book 2

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,11 @@
 class Customer:
-    """A member of Kennels. Stores information on a customer 
+    """A member of Kennels. Stores information on a customer
     that is currently pertinent.
     """
-    def __init__(self, id, name):
+
+    def __init__(self, id, name, address, email, password):
         self.id = id
         self.full_name = name
+        self.address = address
+        self.email = email
+        self.password = password
--- views/customer_requests.py
+++ views/customer_requests.py
@@ -1,8 +1,47 @@
+from models import Customer
+import sqlite3
+import json
+
 CUSTOMERS = [{"id": 1, "name": "Ryan Tanay"}]
 
 
+# def get_all_customers():
+#     return CUSTOMERS
+
+
 def get_all_customers():
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_curser = conn.cursor()
+
+        db_curser.execute(
+            """
+        SELECT
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        FROM customer c
+        """
+        )
+
+        customers = []
+
+        dataset = db_curser.fetchall()
+
+        for row in dataset:
+            customer = Customer(
+                row["id"],
+                row["name"],
+                row["address"],
+                row["email"],
+                row["password"],
+            )
+
+            customers.append(customer.__dict__)
+
+        return customers
 
 
 def get_single_customer(id):
